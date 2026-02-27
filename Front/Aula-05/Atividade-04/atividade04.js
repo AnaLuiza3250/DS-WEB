a25cad34bf0fecba4b1745ccee1b1773d781c64f
AnaLuiza3250 <ferreiraanaluiza346@gmail.com>
2026-02-27 09:31:48 -0300

    Atividade 04 - DOM

--- Front/Aula-05/Atividade-04/atividade04.js
+++ Front/Aula-05/Atividade-04/atividade04.js
@@ -6,7 +6,7 @@ foto.addEventListener("click", function () {
     mensagem.textContent = "Clicou na foto, mesmo sabendo que não podia clicar nela!(Clique novamente, te desafio!";
 
     if (foto.src.includes("download.jpg")) {
-        mensagem.textContent = "Me diga seu nome! (Clique o botão direito do mouse)";
+        mensagem.textContent = "Me diga seu nome! (Digite e clique o botão direito do mouse)";
     }
 });
 
@@ -20,18 +20,31 @@ document.addEventListener("keydown", function (event) {
     saida.textContent = texto;
 });
 
-foto.addEventListener("contextmenu", function (event) {  //substitui a função do clique direito 
-    event.preventDefault();                              //do mouse pelo minha ação personalizada
-    alert("Você é uma pessoa teimosa " + texto + "!");
+foto.addEventListener("contextmenu", function (event) {  //substitui a função do clique direito do mouse pelo minha ação personalizada
+    event.preventDefault();                              
+    let teimoso = prompt("Você é uma pessoa muito teimosa não é? Sim ou Não?");
+    if(teimoso == "Sim" || teimoso == "sim"){
+        alert("Eu sabia que você era teimoso, " + texto + "!");
+    } else if(teimoso == "Não" || teimoso == "não" || teimoso == "Nao" || teimoso == "nao"){
+        alert("Pare de mentir para si mesmo, " + texto + "!");
+    } else {
+        alert("Não sabe nem mesmo responder uma pergunta simples " + texto + "! Melhore!");
+    }
 });
 
 var area = document.getElementById("area");
 area.addEventListener("mouseenter", function () {
+    document.getElementById("quadrado").style.backgroundColor = "red";
+
     mensagem.textContent = "NÃO CLIQUE!";
 
     if (foto.src.includes("download.jpg")) {
         mensagem.textContent = "Clicou na foto, mesmo sabendo que não podia clicar nela!";
     }
+});
+
+area.addEventListener("mouseleave", function () {
+    document.getElementById("quadrado").style.backgroundColor = "rgb(29, 181, 21)";
 });
 
 area.addEventListener("mouseleave", function () {
@@ -41,4 +54,15 @@ area.addEventListener("mouseleave", function () {
         mensagem.textContent = "Clicou na foto, mesmo sabendo que não podia clicar nela!";
     }
 });
+
+var quadrado = document.getElementById("quadrado");
+var x = 0, y = 0;
+document.addEventListener("mousemove", function(event) {
+    y = event.clientY;
+    x = event.clientX;
+});
+setInterval(function () {
+    quadrado.style.top = y + "px";
+    quadrado.style.left = x + "px";
+}, 60);
 
